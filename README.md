5519ce040eaa3229f78ab28775e422b94ef2abaa
Aruna <admgbit@gmail.com>
2019-01-11 12:58:07 +0530

    Save file Exteranl Storage

---
+++ README.md
@@ -0,0 +1,1 @@
+# Android Camera And Save File in WRITE_EXTERNAL_STORAGE
--- app/src/main/java/com/aruna/camera/MainActivity.java
+++ app/src/main/java/com/aruna/camera/MainActivity.java
@@ -87,10 +87,45 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = iv_photo.getWidth();
+        int targetH = iv_photo.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        iv_photo.setImageBitmap(bitmap);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         File f = image;
         Bitmap myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
-        iv_photo.setImageBitmap(myBitmap);
+//        iv_photo.setImageBitmap(myBitmap);
+
+        galleryAddPic();
+        setPic();
     }
 }
